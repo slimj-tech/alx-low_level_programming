e23f32b5a22a4c46b21db835779052b5ec262614
slimj-tech <mmesomaokwor@gmail.com>
2022-09-08 16:46:20 -0700

    four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main(void)
 {
-	char mystr[]="\"Programming is like building a multilingual puzzle\n";
+	char mystr[] = "\"Programming is like building a multilingual puzzle";
 
-	puts(mystr);
+	puts (mystr);
 	return(0);
 }
