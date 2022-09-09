a427246cdb8d2b6f798ef65902ecbe3f12ccde10
slimj-tech <mmesomaokwor@gmail.com>
2022-09-08 18:24:20 -0700

    five

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,10 +6,10 @@
  * this prints "with proper grammer, but the out come"
  * Return: 0
  */
-int main (void)
+int main(void)
 { 
 	char mystring = ("with proper grammar, but the outcome
 		       	is a piece of art,");
 	printf(mystring);
-	return(0)
+	return (0);
 }
