d4e7948b7ec073b04c2e08421a9a05f8d0d5c0bb
slimj-tech <mmesomaokwor@gmail.com>
2022-09-08 18:19:15 -0700

    five

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this prints "with proper grammer, but the out come"
+ * Return: 0
+ */
+int main (void)
+{ 
+	char mystring = ("with proper grammar, but the outcome
+		       	is a piece of art,");
+	printf(mystring);
+	return(0)
+}
