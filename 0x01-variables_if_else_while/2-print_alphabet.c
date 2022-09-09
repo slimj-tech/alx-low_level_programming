08204f6765b14e145851965aa30647b6a859f728
slimj-tech <mmesomaokwor@gmail.com>
2022-09-09 15:51:22 -0700

    wp

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- print alphsbets
+ *
+ * Return: true
+ */
+int main (void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar ('\n');
+	return (0)
+}
