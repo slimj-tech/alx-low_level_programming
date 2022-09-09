4f09466a2e04971d42ae6c42895daeeea982eda9
slimj-tech <mmesomaokwor@gmail.com>
2022-09-09 16:31:08 -0700

    gh

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main-alphabets
+ *
+ * Return: true
+ */
+int main(void)
+{
+	char ff;
+
+	for(ff = 'a'; ff <= 'z'; ff++)
+		if (ff != 'q' && ff != 'e')
+		{
+			putchar(ff);
+		}
+	putchar('\n');
+	return (0);
+}
