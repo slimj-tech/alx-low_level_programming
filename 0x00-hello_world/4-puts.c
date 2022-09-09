58021b100f933deb595c487b7aac750f465a617b
slimj-tech <mmesomaokwor@gmail.com>
2022-09-08 17:55:30 -0700

    zero

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 /**
  *main:this is a function that prints a string
+ *
+ * Description: this program prints the string
+ *return:0
  */
 int main(void)
 {
 	char mystr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(mystr);
-	/**
-	 * return: returns the string
-	 */
 	return (0);
 }
