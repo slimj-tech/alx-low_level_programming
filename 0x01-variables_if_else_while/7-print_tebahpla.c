9a30d5c8fe74d2ea3714d635474607f783f909e4
slimj-tech <mmesomaokwor@gmail.com>
2022-09-09 17:14:11 -0700

    da

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- alphabets in reverse
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int alp;
+
+	for(alp = z; alp >= a; alp++)
+		putchar(alp);
+	putchar('\n');
+	return (0);
+}
