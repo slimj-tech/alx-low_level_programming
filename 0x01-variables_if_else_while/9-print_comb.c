272f908b63d2e968664089fcc0541b40bda4810f
slimj-tech <mmesomaokwor@gmail.com>
2022-09-11 04:05:00 -0700

    nine

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,16 +7,16 @@
 int main(void)
 {
 	int nums;
-
+	
 	for (nums = 0; nums <= 9; nums++)
+	{
+		putchar(nums + '0');
+		if (nums < 9)
 		{
-			putchar(nums + '0');
-	if (nums < 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(',');
+			putchar(' ');
 		}
+	}
 	putchar('\n');
 	return (0);
 }
