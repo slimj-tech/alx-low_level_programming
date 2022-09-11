626df53275036f071d9b40a59fe8de476ec8c8eb
slimj-tech <mmesomaokwor@gmail.com>
2022-09-11 03:08:05 -0700

    nine

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main- combination
+ *
+ * Return: success
+ */
+int main(void)
+{
+	int nums;
+
+	for (nums = 0; nums <= 9; nums++)
+		{
+			putchar(nums + '0');
+			if (nums < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	putchar('\n');
+	return (0)
+}
