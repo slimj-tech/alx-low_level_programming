ae95e2791a7e6d2922f17520a2278cc776a6b9bb
slimj-tech <mmesomaokwor@gmail.com>
2022-09-12 20:17:32 -0700

    ten

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,14 +11,14 @@ int main(void)
 
 	for (a = '0'; a <= '9'; a++)
 	{
-		for (b = '0'; b < '9'; b++)
+		for (b = '0'; b <= '9'; b++)
 		{
 			if (a < b)
 			{
 				putchar(a);
 				putchar(b);
 
-			if (a != '8' || (a == '8' && b != '9'))
+			if (a != '8')
 			{
 				putchar(',');
 				putchar(' ');
