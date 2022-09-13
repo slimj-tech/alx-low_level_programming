88cab4b3317ff93f269c65c903bb75412aced9e1
slimj-tech <mmesomaokwor@gmail.com>
2022-09-12 19:55:15 -0700

    ten

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main-number combination
+ *
+ * Result: always success
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b < '9'; b++)
+		{
+			if (a < b)
+			{
+				putchar('a');
+				putchar('b');
+
+			if (a != '8')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
