36f22866ea030e8602c0773d3c2bd8902e6aafd4
slimj-tech <mmesomaokwor@gmail.com>
2022-09-14 07:57:39 -0700

    one

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+ * print_alphabets- prints alphabets in lowercase
+ *
+ * Return- 0
+ */
+void print_alphabet(void)
+{
+	int j;
+	
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
