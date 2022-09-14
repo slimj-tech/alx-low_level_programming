3fb208af192628798639055719bb93572e29f632
slimj-tech <mmesomaokwor@gmail.com>
2022-09-14 12:06:14 -0700

    three

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _islower -> checks if the character is lowercase
+* @c: a charcter argument
+* Return: returns 1 and 0 depending on condition
+*/
+
+int _islower(int c)
+{
+	if(c >= 'a' && c <= 'z')
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+}
+
