8026d499f3a9431165099e6b99b4fbd4c694deac
slimj-tech <mmesomaokwor@gmail.com>
2022-09-14 01:15:17 -0700

    zero

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,8 +5,16 @@
  *
  * Return:0 success
  */
-char main(void)
+int main(void)
 {
-	putchar("_putchar\n")
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
