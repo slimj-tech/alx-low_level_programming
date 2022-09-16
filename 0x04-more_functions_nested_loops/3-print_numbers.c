aaf5a2be73efa0dd77c64a773fa3d4ec498509ef
slimj-tech <mmesomaokwor@gmail.com>
2022-09-16 22:33:49 +0000

    three

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * void print_numbers(void)- this prints numbs
+ * Return: success 0
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	return (10);
+}
