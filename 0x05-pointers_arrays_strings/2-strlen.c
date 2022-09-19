83af9ad612553d6e18d6d01efeb3a00930d3cc97
slimj-tech <mmesomaokwor@gmail.com>
2022-09-19 20:19:52 +0000

    one

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+nclude "main.h"
+/**
+ *_strlen - returns the lenght of a string
+ *@s: string
+ *Return: success
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
+}
