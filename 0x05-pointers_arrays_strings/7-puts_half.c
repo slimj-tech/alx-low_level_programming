49f6358f84bcaae760f28241d70111f78d42e32a
slimj-tech <mmesomaokwor@gmail.com>
2022-09-20 21:55:24 +0000

    projects

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *puts_half - prints half of string
+ *Return: length of string
+ *@str: string
+ */
+void puts_half(char *str)
+{
+	int c = 0;
+
+	int i;
+
+	while (str[c] != 0)
+	{
+		c++;
+	}
+	for (i = 0; i < c; i++)
+	{
+		if (c % 2 == 0 && i >= c / 2)
+			_putchar(str[i]);
+		else if (c % 2 != 0 && i >= c - ((c - 1) / 2))
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
