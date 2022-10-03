e0950ed766897aa71f5cb9aad9e4f79b54ea4dff
slimj-tech <mmesomaokwor@gmail.com>
2022-10-03 08:11:49 +0000

    first

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies args
+ * Return: 0
+ * @argc: amount of args
+ * @argv: pointer to pointers array
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int result = 1;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (argc < 3)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			int x = strtol(argv[i], NULL, 10);
+			result = result * x;
+		}
+	}
+	printf("%d\n", result);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+int _putchar(char c);
+int convert_to_integer(char *s);
+int _atoi(char *s);
+int _strlen_recursion(char *s);
+int only_nums(char *s);
+#endif
