4f019b198ffe9ec007353b3fc00dad7295b4881f
slimj-tech <mmesomaokwor@gmail.com>
2022-10-07 00:17:59 +0000

    first

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,7 +17,7 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	
+
 	while (s1[len1])
 		len1++;
 	while (s2[len2])
@@ -29,8 +29,10 @@ char *str_concat(char *s1, char *s2)
 		if (s == NULL)
 			return (NULL);
 
-	for (i = 0; i < (len1 + len2); i++)
-		s[i] = s1[i] + s2[i];
+	for (i = 0; i < len1; i++)
+		s[i] = s1[i];
+	for (i = 0; i < len2; I++)
+		s[len1 + i] = s[2];
 	s[len1 + len2] = 0;
 	return (s);
 	/*for (i = 0; i < len2; i++)*/
