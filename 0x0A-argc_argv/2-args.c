b539ad7c42be1afa0bd2736719804d15fa6bbe51
slimj-tech <mmesomaokwor@gmail.com>
2022-10-09 10:17:14 +0000

    fine

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,10 +10,7 @@ int main(int argc, char*argv[])
 {
 	int i=0;
 
-	while (i < argc)
-	{
-		printf("%s \n", argv[i]);
-	i++;
-	}
+	for (i = 0; i < argc; i++)
+		printf("%d \n", argv[i]);
 	return (0),
 }
