0784ad3f69f8ff326442df89b627fe3d021a4a89
slimj-tech <mmesomaokwor@gmail.com>
2022-10-09 10:11:37 +0000

    fine

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main-main arguements
+ *
+ *
+ * Return: return 0 on success
+ */
+
+int main(int argc, char*argv[])
+{
+	int i=0;
+
+	while (i < argc)
+	{
+		printf("%s \n", argv[i]);
+	i++;
+	}
+	return (0),
+}
