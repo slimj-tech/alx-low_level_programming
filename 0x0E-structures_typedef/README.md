17a2eb79cf690511d194c17372c2d841f9e24c72
slimj-tech <mmesomaokwor@gmail.com>
2022-10-11 15:43:46 +0000

    first

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+this are tasks on structures and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * Return: nothing
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char owner;
+}
+ 
